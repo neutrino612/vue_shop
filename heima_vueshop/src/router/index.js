0a9860392d384a8cecedcc2a1c613efc5f979878
neutrino612 <251963120@qq.com>
2022-06-21 07:52:14 +0800

    完成了报表功能的开发

--- heima_vueshop/src/router/index.js
+++ heima_vueshop/src/router/index.js
@@ -8,6 +8,7 @@ import Welcome from '../components/Welcome'
 import Users from '../components/user/Users.vue'
 import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
+import Report from '../components/report/Report.vue'
 
 Vue.use(VueRouter)
 
@@ -21,8 +22,8 @@ const routes = [
     { path:'/welcome',component: Welcome },
     { path:'/users',component: Users},
     { path:'/rights',component: Rights},
-    { path:'/roles',component: Roles}
-
+    { path:'/roles',component: Roles},
+    { path:'/reports',component: Report}
   ]
   }
 
